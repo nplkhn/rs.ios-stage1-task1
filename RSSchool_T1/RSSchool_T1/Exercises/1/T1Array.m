3a30bdfbde41c49ae97f1f3f86182b2e6018729d
Nikita Plakhin <nplkhn1@yandex.ru>
2020-03-16 16:06:45 +0300

    feat: implement task T1Array

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,24 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    if (sadArray.count < 3){
+        return sadArray;
+    }
+    NSMutableArray* happyArray = [NSMutableArray arrayWithArray:sadArray];
+    for (NSUInteger i = 1; i < happyArray.count - 1; i++) {
+        NSNumber* intNumber = happyArray[i-1];
+        int left = [intNumber intValue];
+        intNumber = happyArray[i+1];
+        int right = [intNumber intValue];
+        intNumber = happyArray[i];
+        int mid = [intNumber intValue];
+        if (left + right < mid) {
+            [happyArray removeObjectAtIndex:i];
+            i = 1;
+        }
+    }
+    
+    return happyArray;
 }
 
 @end
