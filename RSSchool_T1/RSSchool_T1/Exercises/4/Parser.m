17f5152dca4bab31ac0ee640743c6197ab66005d
Nikita Plakhin <nplkhn1@yandex.ru>
2020-03-16 23:57:52 +0300

    feat: implement partial solution of task4 Parser

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,7 +4,28 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    return @[@"Test"];
+    NSMutableString* newString = [NSMutableString stringWithString:string];
+    for (NSUInteger i = 0; i < newString.length; i++){
+        if ([[newString substringWithRange:NSMakeRange(i, 1)] isEqualToString:@"["]) {
+            [newString replaceCharactersInRange:NSMakeRange(i, 1) withString:@"<"];
+        } else if ([[newString substringWithRange:NSMakeRange(i, 1)] isEqualToString:@"]"]) {
+        [newString replaceCharactersInRange:NSMakeRange(i, 1) withString:@">"];
+        }
+    }
+    NSMutableArray* substrings = [NSMutableArray new];
+    NSError *error = nil;
+    NSRegularExpression *regexp = [NSRegularExpression regularExpressionWithPattern:@"([<(].*?[>)])"
+                                                                            options:NSRegularExpressionCaseInsensitive
+                                                                              error:&error];
+    NSArray* matches = [regexp matchesInString:newString
+                                       options:0
+                                         range:NSMakeRange(0, string.length)];
+    for (NSTextCheckingResult *matchResult in matches) {
+        NSString* match = [newString substringWithRange:matchResult.range];
+        match = [match substringWithRange:NSMakeRange(1, match.length - 2)];
+        [substrings addObject:match];
+    }
+    return substrings;
 }
 
 @end
