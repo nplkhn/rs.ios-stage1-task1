bdcfbdc5425e5e2cc54163d7ed82eb512498dd4d
Nikita Plakhin <nplkhn1@yandex.ru>
2020-03-16 20:45:02 +0300

    feat: implement task BillCounter

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,6 +4,22 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
+    int total_cost = 0;
+    for (NSUInteger i = 0; i < bill.count; i++)
+    {
+        if (i != index){
+            NSNumber* cost = bill[i];
+            total_cost += [cost intValue];
+        }
+    }
+    total_cost /= 2;
+    if (total_cost == [sum intValue])
+    {
+        return @"Bon Appetit";
+    } else {
+        return [NSString stringWithFormat:@"%d", [sum intValue] - total_cost];
+    }
+    
     return @"";
 }
 
