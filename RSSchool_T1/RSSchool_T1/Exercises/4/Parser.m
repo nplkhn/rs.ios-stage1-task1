3e858c8dca437a70f4c858472e2de482d78539af
Nikita Plakhin <nplkhn1@yandex.ru>
2020-03-17 20:46:55 +0300

    fix: implement checking for not parsed strings

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,21 +4,29 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    NSMutableString* newString = [NSMutableString stringWithString:string];
     NSMutableArray* substrings = [NSMutableArray new];
     NSError *error = nil;
-    NSRegularExpression *regexp = [NSRegularExpression regularExpressionWithPattern:@"(<.*?>)|(\\[.*?\\])|(\\(.*?\\))"
+    NSRegularExpression *regex1 = [NSRegularExpression regularExpressionWithPattern:@"(<.*?>)|(\\(.*?\\))|(\\[.*?\\])"
                                                                             options:NSRegularExpressionCaseInsensitive
                                                                               error:&error];
-    NSArray* matches = [regexp matchesInString:newString
+
+    NSArray* matches = [regex1 matchesInString:string
                                        options:0
                                          range:NSMakeRange(0, string.length)];
     for (NSTextCheckingResult *matchResult in matches) {
-        NSString* match = [newString substringWithRange:matchResult.range];
+        NSString* match = [string substringWithRange:matchResult.range];
         match = [match substringWithRange:NSMakeRange(1, match.length - 2)];
         [substrings addObject:match];
     }
     
+    for (NSUInteger i = 0; i < substrings.count; i++) {
+        NSArray<NSString*>* subs = [NSArray arrayWithArray:[[Parser new] parseString:substrings[i]]];
+        for (NSString* sub in subs) {
+            if (![substrings containsObject:sub]) {
+                [substrings addObject:sub];
+            }
+        }
+    }
     
     
     return substrings;
--- RSSchool_T1/RSSchool_T1Tests/StringParseTests.m
+++ RSSchool_T1/RSSchool_T1Tests/StringParseTests.m
@@ -21,6 +21,7 @@
 - (void)testParser3 {
     NSString *inputString = @"Lorem <(ipsum [dolor <sit] amet), consectetur adipiscing elit>. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet>. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget (nulla. Class aptent <taciti [sociosqu ad] litora torquent per conubia> nostra), per inceptos himenaeos.";
     NSArray *parsedArray = [[[Parser alloc] init] parseString:inputString];
+    NSLog(@"%@", parsedArray);
     XCTAssertTrue([parsedArray containsObject:@"(ipsum [dolor <sit] amet), consectetur adipiscing elit>. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet"]);
     XCTAssertTrue([parsedArray containsObject:@"ipsum [dolor <sit] amet"]);
     XCTAssertTrue([parsedArray containsObject:@"(ipsum [dolor <sit] amet), consectetur adipiscing elit>. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet"]);
