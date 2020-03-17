c0f60e5791abbccd1c72924d87428fa5724079d1
Nikita Plakhin <nplkhn1@yandex.ru>
2020-03-17 23:22:48 +0300

    fix: implement correct solution of task 4 - StringParser

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -5,27 +5,64 @@
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
     NSMutableArray* substrings = [NSMutableArray new];
-    NSError *error = nil;
-    NSRegularExpression *regex1 = [NSRegularExpression regularExpressionWithPattern:@"(<.*?>)|(\\(.*?\\))|(\\[.*?\\])"
-                                                                            options:NSRegularExpressionCaseInsensitive
-                                                                              error:&error];
-
-    NSArray* matches = [regex1 matchesInString:string
-                                       options:0
-                                         range:NSMakeRange(0, string.length)];
-    for (NSTextCheckingResult *matchResult in matches) {
-        NSString* match = [string substringWithRange:matchResult.range];
-        match = [match substringWithRange:NSMakeRange(1, match.length - 2)];
-        [substrings addObject:match];
+    for (NSUInteger i = 0; i < string.length; i++) {
+        int counter = 0;
+        NSUInteger endPos = 0;
+        if ([[string substringWithRange:NSMakeRange(i, 1)] isEqualToString: @"("]) {
+            NSString* openingBracket = @"(";
+            NSString* closingBracket = @")";
+            counter++;
+            for (NSUInteger j = i + 1; j < string.length; j++) {
+                if ([[string substringWithRange:NSMakeRange(j, 1)] isEqualToString:openingBracket]) {
+                    counter++;
+                } else if ([[string substringWithRange:NSMakeRange(j, 1)] isEqualToString:closingBracket]) {
+                    counter--;
+                }
+                if (counter == 0) {
+                    endPos = j;
+                    break;
+                }
+            }
+            [substrings addObject:[string substringWithRange:NSMakeRange(i, endPos - i)]];
+        } else if ([[string substringWithRange:NSMakeRange(i, 1)] isEqual: @"["]) {
+            NSString* openingBracket = @"[";
+            NSString* closingBracket = @"]";
+            counter++;
+            for (NSUInteger j = i + 1; j < string.length; j++) {
+                if ([[string substringWithRange:NSMakeRange(j, 1)] isEqualToString:openingBracket]) {
+                    counter++;
+                } else if ([[string substringWithRange:NSMakeRange(j, 1)] isEqualToString:closingBracket]) {
+                    counter--;
+                }
+                if (counter == 0) {
+                    endPos = j;
+                    break;
+                }
+            }
+            [substrings addObject:[string substringWithRange:NSMakeRange(i, endPos - i)]];
+            
+        } else if ([[string substringWithRange:NSMakeRange(i, 1)] isEqual: @"<"]) {
+            NSString* openingBracket = @"<";
+            NSString* closingBracket = @">";
+            counter++;
+            for (NSUInteger j = i + 1; j < string.length; j++) {
+                if ([[string substringWithRange:NSMakeRange(j, 1)] isEqualToString:openingBracket]) {
+                    counter++;
+                } else if ([[string substringWithRange:NSMakeRange(j, 1)] isEqualToString:closingBracket]) {
+                    counter--;
+                }
+                if (counter == 0) {
+                    endPos = j;
+                    break;
+                }
+            }
+            [substrings addObject:[string substringWithRange:NSMakeRange(i, endPos - i)]];
+        }
     }
     
     for (NSUInteger i = 0; i < substrings.count; i++) {
-        NSArray<NSString*>* subs = [NSArray arrayWithArray:[[Parser new] parseString:substrings[i]]];
-        for (NSString* sub in subs) {
-            if (![substrings containsObject:sub]) {
-                [substrings addObject:sub];
-            }
-        }
+        NSString* temp = substrings[i];
+        substrings[i] = [temp substringWithRange:NSMakeRange(1, temp.length - 1)];
     }
     
     
