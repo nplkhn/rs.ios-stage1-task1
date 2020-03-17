700e16fde0e5238b014f16961bfd9b3d5a1a6058
Nikita Plakhin <nplkhn1@yandex.ru>
2020-03-17 19:31:02 +0300

    fix: implement correct regex

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -5,16 +5,9 @@
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
     NSMutableString* newString = [NSMutableString stringWithString:string];
-    for (NSUInteger i = 0; i < newString.length; i++){
-        if ([[newString substringWithRange:NSMakeRange(i, 1)] isEqualToString:@"["]) {
-            [newString replaceCharactersInRange:NSMakeRange(i, 1) withString:@"<"];
-        } else if ([[newString substringWithRange:NSMakeRange(i, 1)] isEqualToString:@"]"]) {
-        [newString replaceCharactersInRange:NSMakeRange(i, 1) withString:@">"];
-        }
-    }
     NSMutableArray* substrings = [NSMutableArray new];
     NSError *error = nil;
-    NSRegularExpression *regexp = [NSRegularExpression regularExpressionWithPattern:@"([<(].*?[>)])"
+    NSRegularExpression *regexp = [NSRegularExpression regularExpressionWithPattern:@"(<.*?>)|(\\[.*?\\])|(\\(.*?\\))"
                                                                             options:NSRegularExpressionCaseInsensitive
                                                                               error:&error];
     NSArray* matches = [regexp matchesInString:newString
@@ -25,7 +18,11 @@
         match = [match substringWithRange:NSMakeRange(1, match.length - 2)];
         [substrings addObject:match];
     }
+    
+    
+    
     return substrings;
 }
 
 @end
+
